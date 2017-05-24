a71ceb0a9aba5b6d53eecd3b710029b8f52d3f6e
Guiliang <gla68@sfu.ca>
2017-05-24 16:41:26 -0700

    2017-5-24

---
+++ td_prediction_simple_dir/agg2calibrate_entire_dataset_separated_model.py
@@ -0,0 +1,78 @@
+import os
+import scipy.io as sio
+import unicodedata
+import ast
+
+FEATURE_TYPE = 5
+calibration_store_dir = "/media/gla68/Windows/Hockey-data/calibrate_all_feature_" + str(FEATURE_TYPE)
+ISHOME = True
+check_target = {"GD": 0, "MD": 0, "P": 1}
+
+
+def agg2calibrate_model():
+
+    model_predict_value_record = []
+    calibration_value_record = []
+
+    for calibration_dir_game in os.listdir(calibration_store_dir):
+        for file_name in os.listdir(calibration_store_dir + "/" + calibration_dir_game):
+            if "training_data_dict_all_value" in file_name:
+                calibrate_value_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "training_data_dict_all_name" in file_name:
+                calibrate_name_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "model_predict_home" in file_name:
+                model_predict_home_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "model_predict_away" in file_name:
+                model_predict_away_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "home_identifier" in file_name:
+                home_identifier_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "summation_goal_home" in file_name:
+                summation_goal_home_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            elif "summation_goal_away" in file_name:
+                summation_goal_away_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+            else:
+                continue
+
+        calibrate_values = ((sio.loadmat(calibrate_value_name))["training_data_dict_all_value"]).tolist()
+        calibrate_names = ((sio.loadmat(calibrate_name_name))["training_data_dict_all_name"]).tolist()
+        model_predict_home = ((sio.loadmat(model_predict_home_name))["model_predict_home"]).tolist()
+        model_predict_away = ((sio.loadmat(model_predict_away_name))["model_predict_away"]).tolist()
+        home_identifier = (((sio.loadmat(home_identifier_name))["home_identifier"])[0]).tolist()
+        summation_goal_home = ((sio.loadmat(summation_goal_home_name))["summation_goal_home"]).tolist()
+        summation_goal_away = ((sio.loadmat(summation_goal_away_name))["summation_goal_away"]).tolist()
+
+        for calibrate_name_index in range(0, len(calibrate_names)):
+            calibrate_name = calibrate_names[calibrate_name_index]
+            calibrate_name_str = unicodedata.normalize('NFKD', calibrate_name).encode('ascii', 'ignore')
+            calibrate_name_dict = ast.literal_eval(calibrate_name_str)
+            goal_diff = calibrate_name_dict.get("scoreDifferential")
+            manpower_diff = calibrate_name_dict.get("Penalty")
+            time_remain = calibrate_name_dict.get("time remained")
+            if time_remain > 2400:
+                period = 1.0
+            elif time_remain > 1200:
+                period = 2.0
+            elif time_remain > 0:
+                period = 3.0
+            else:
+                period = 4.0
+
+            if float(check_target.get("GD")) == float(goal_diff) and float(check_target.get("MD")) == float(manpower_diff) and float(check_target.get("P")) == float(period):
+                if ISHOME and home_identifier[calibrate_name_index]:
+                    # print "Found home"
+                    model_predict_value_record.append(model_predict_home[calibrate_name_index])
+                    calibration_value_record.append(float(summation_goal_home[calibrate_name_index]))
+                elif not ISHOME and not home_identifier[calibrate_name_index]:
+                    # print "Found away"
+                    model_predict_value_record.append(model_predict_away[calibrate_name_index])
+                    calibration_value_record.append(float(summation_goal_away[calibrate_name_index]))
+
+    model_predict_value_average = float(sum(model_predict_value_record))/len(model_predict_value_record)
+    calibration_value_record_average = float(sum(calibration_value_record))/len(calibration_value_record)
+
+    print "model_predict_value_average: "+str(model_predict_value_average)
+    print "calibration_value_record_average: " + str(calibration_value_record_average)
+
+
+if __name__ == '__main__':
+    agg2calibrate_model()
---
+++ td_prediction_simple_dir/calibrate_predict_entire_dataset_separated_model.py
@@ -0,0 +1,77 @@
+import scipy.io as sio
+import tensorflow as tf
+import os
+import unicodedata
+import ast
+import td_prediction_simple_separated
+import numpy as np
+
+FEATURE_TYPE = 5
+calibration = True
+ISHOME = False
+
+SIMPLE_HOME_SAVED_NETWORK_PATH = "/home/gla68/PycharmProjects/Sport-Analytic-NN/td_prediction_simple_dir/saved_NN/saved_Home_networks_feature" + str(
+    FEATURE_TYPE) + "_batch16_iterate2Converge-NEG_REWARD_GAMMA1_V3-Sequenced_bak"
+SIMPLE_AWAY_SAVED_NETWORK_PATH = "/home/gla68/PycharmProjects/Sport-Analytic-NN/td_prediction_simple_dir/saved_NN/saved_Away_networks_feature" + str(
+    FEATURE_TYPE) + "_batch16_iterate2Converge-NEG_REWARD_GAMMA1_V3-Sequenced_bak"
+
+calibration_store_dir = "/media/gla68/Windows/Hockey-data/calibrate_all_feature_" + str(FEATURE_TYPE)
+
+sess_nn = tf.InteractiveSession()
+model_nn = td_prediction_simple_separated.td_prediction_simple_V3()
+saver = tf.train.Saver()
+sess_nn.run(tf.global_variables_initializer())
+
+if ISHOME:
+    SIMPLE_SAVED_NETWORK_PATH = SIMPLE_HOME_SAVED_NETWORK_PATH
+else:
+    SIMPLE_SAVED_NETWORK_PATH = SIMPLE_AWAY_SAVED_NETWORK_PATH
+
+checkpoint = tf.train.get_checkpoint_state(SIMPLE_SAVED_NETWORK_PATH)
+if checkpoint and checkpoint.model_checkpoint_path:
+    saver.restore(sess_nn, checkpoint.model_checkpoint_path)
+    print("Successfully loaded:", checkpoint.model_checkpoint_path)
+else:
+    raise Exception("can't restore network")
+
+for calibration_dir_game in os.listdir(calibration_store_dir):
+    for file_name in os.listdir(calibration_store_dir + "/" + calibration_dir_game):
+        if "training_data_dict_all_value" in file_name:
+            calibrate_value_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+        elif "training_data_dict_all_name" in file_name:
+            calibrate_name_name = calibration_store_dir + "/" + calibration_dir_game + "/" + file_name
+        else:
+            continue
+
+    calibrate_values = (sio.loadmat(calibrate_value_name))["training_data_dict_all_value"]
+    calibrate_names = (sio.loadmat(calibrate_name_name))["training_data_dict_all_name"]
+
+    home_identifier = []
+    for calibrate_name in calibrate_names:
+        calibrate_name_str = unicodedata.normalize('NFKD', calibrate_name).encode('ascii', 'ignore')
+        calibrate_name_dict = ast.literal_eval(calibrate_name_str)
+        if calibrate_name_dict.get("home"):
+            home_identifier.append(1)
+        else:
+            home_identifier.append(0)
+
+    readout_t1_batch = model_nn.read_out.eval(feed_dict={model_nn.x: calibrate_values})  # get value of s
+
+    if ISHOME:
+        data_name = "model_predict_home"
+    else:
+        data_name = "model_predict_away"
+
+    sio.savemat(calibration_store_dir + "/" + calibration_dir_game+"/" + "home_identifier",
+                {"home_identifier": home_identifier})
+    sio.savemat(calibration_store_dir + "/" + calibration_dir_game + "/" + data_name,
+                {data_name: np.asarray(readout_t1_batch)})
+
+    print readout_t1_batch
+
+
+# def check_calibrate_home_away(calibrate_name):
+
+
+# if __name__ == '__main__':
+#     print "abc"
