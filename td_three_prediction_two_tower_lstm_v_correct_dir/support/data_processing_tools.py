63db20820cf38aea539d2b6748d1ba7c33f4bc49
Guiliang <gla68@sfu.ca>
2019-04-22 19:43:11 -0700

    Update data_processing_tools.py
    add game name

--- td_three_prediction_two_tower_lstm_v_correct_dir/support/data_processing_tools.py
+++ td_three_prediction_two_tower_lstm_v_correct_dir/support/data_processing_tools.py
@@ -446,10 +446,10 @@ def find_game_dir(dir_all, data_path, target_game_id, sports='IceHockey'):
             gameId = str(data.get('gameId'))
             # gameId = unicodedata.normalize('NFKD', gameId).encode('ascii', 'ignore')
             print str(data.get('gameDate'))
-            # if gameId == target_game_id:
-            #     game_name = directory
-            #     print directory
-            #     break
+            if gameId == target_game_id:
+                game_name = directory
+                print directory
+                break
     else:
         raise ValueError('Unknown sports game')
 
